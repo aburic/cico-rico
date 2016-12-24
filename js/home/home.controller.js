1694b00c9381826c0f98ed708d962c36dea26192
aleksandraburic <alexandraburic91@gmail.com>
2016-12-24 22:42:25 +0100

    message

---
+++ js/home/home.controller.js
@@ -0,0 +1,14 @@
+(function(){
+
+  'use strict';
+
+angular.module('home')
+.controller('HomeController', HomeController);
+
+
+HomeController.$inject = ['$location'];
+function HomeController($location) {
+  var vm = this;
+}
+
+})();
